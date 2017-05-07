002ea34778dfa510c9be10c9d683870f1f60b5f0
Anmol POrwal <anmolporwal14@gmail.com>
2017-05-07 22:31:32 +0530

    code4

--- codeforces/R412/qC.cpp
+++ codeforces/R412/qC.cpp
@@ -3,7 +3,40 @@ using namespace std;
 
 int main()
 {
+int t;
+cin >> t;
+for(int i=0;i<t;i++)
+{
+int x,y,p,q;
+cin >> x >> y >> p >> q ;
+if(p == q )
+{  
+   if(y == x)
+	   cout << 0 << endl;
+   else
+	cout << -1 << endl;
+continue;
+}
+if( x==p && x==0  )
+{cout <<  0 << endl;
+	continue;}
+if ( p==0 && x!=0)
+{
+  cout << -1 << endl;
+  continue;
+}
 
+int qf=q;
+int pf=p;
+
+while( x > pf || y > qf || ( pf-x > qf-y ) )
+{
+  pf+=p;
+  qf+=q;
+}
+
+cout << qf-y << endl;
+}
 
 
 
