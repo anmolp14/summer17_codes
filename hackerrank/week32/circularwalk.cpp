6913511dcd49e69355b6c79b085b972caf512834
anmolp14 <anmolporwal14@gmail.com>
2017-05-17 15:05:15 +0530

    week32 q3

---
+++ hackerrank/week32/circularwalk.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int circularWalk(int n, int s, int t, int r_0, int g, int seed, int p){
+    // Complete this function
+}
+
+int main() {
+    int n;
+    int s;
+    int t;
+    cin >> n >> s >> t;
+    int r_0;
+    int g;
+    int seed;
+    int p;
+    cin >> r_0 >> g >> seed >> p;
+    int result = circularWalk(n, s, t, r_0, g, seed, p);
+    cout << result << endl;
+    return 0;
+}
