8a199bf389d3c724a63e089ae26496369c2c57a3
Anmol POrwal <anmolporwal14@gmail.com>
2017-05-17 18:43:32 +0530

    week32

--- hackerrank/week32/circularwalk_v2.cpp
+++ hackerrank/week32/circularwalk_v2.cpp
@@ -6,17 +6,16 @@ vector <long int> visited,dist;
 void BFS( long int * r , long int s, long int t )
 {long int temp;
     queue <long int> q; q.push(s); dist[s] = 0; visited[s] =1;
-   
-    while(  !q.empty() )
+    while(  !q.empty() && visited[t]==0 )
     {
         temp = q.front(); q.pop();
         for(long int i=temp-r[temp]; i <=temp+r[temp] ; i++)
-        {
-            if(visited[(i%n) + ((i%n<0) ? n:0)]  == 0)
+        {  long int tr = (i%n) + ((i%n<0) ? n:0);
+            if(visited[tr]  == 0)
             {
-                visited[ (i%n) + ((i%n<0) ? n:0) ] =1;
-                dist[  (i%n) + ((i%n<0) ? n:0) ] = dist[temp] + 1;
-                q.push((i%n) + ((i%n<0) ? n:0) );
+                visited[tr] =1;
+                dist[tr] = dist[temp] + 1;
+                q.push(tr);
             }
         }
     }
@@ -27,29 +26,9 @@ int main() {
     cin >> n >> s >> t >> r_0 >> g >> seed >> p;
     long int r[n];r[0] = r_0;
     for( long int i=1;i<n;i++)
-    { r[i] = ((r[i-1] * g)%p + seed%p) % p ;
+    { r[i] = (r[i-1] * g + seed) % p ;
         if(r[i] < 0) r[i] +=p;}
-/*    vector <vector< long int> > G(n);
-
-    for( long int i=0;i<n;i++)
-    {
-        if( r[i] * 2 + 1 > n)
-        {
-            for( long int y=0;y<n;y++)
-                G[i].push_back(y);
-            continue;
-        }
-        long int jl = (i-r[i]);
-        long int jb = (i+r[i]);
-        long int j;
-        for( j=jl; j<=jb; j++)
-        { if(j!=i)
-            G[i].push_back( (j%n) + ((j%n<0) ? n:0) );}
-
-    }
-*/
-   for(long int i=0;i<n ;i++)
-    { visited.push_back(0); dist.push_back(-1);}
+   visited.assign(n,0); dist.assign(n,-1);
 
     BFS(r, s,t);
 
