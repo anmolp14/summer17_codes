3d85e5cb0d6d2f4ea763be90e942e7ad2bfc7fe5
Anmol POrwal <anmolporwal14@gmail.com>
2017-05-17 17:03:15 +0530

    week 32 Q3

--- hackerrank/week32/circularwalk.cpp
+++ hackerrank/week32/circularwalk.cpp
@@ -1,22 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int circularWalk(int n, int s, int t, int r_0, int g, int seed, int p){
-    // Complete this function
+vector <long int> visited,dist;
+void BFS( vector< vector<long int> >* G , long int s, long int t )
+{long int temp;
+    queue <long int> q; q.push(s); dist[s] = 0; visited[s] =1;
+    while(  visited[t]==0 )
+    {
+        temp = q.front(); q.pop();
+        for(long int i=0; i < (*G)[temp].size(); i++)
+        {
+            if(visited[(*G)[temp][i]] == 0)
+            {
+                visited[(*G)[temp][i]] =1; 
+                dist[(*G)[temp][i]] = dist[temp] + 1;
+                q.push((*G)[temp][i]);
+            }
+        }
+    }
 }
-
 int main() {
-    int n;
-    int s;
-    int t;
-    cin >> n >> s >> t;
-    int r_0;
-    int g;
-    int seed;
-    int p;
-    cin >> r_0 >> g >> seed >> p;
-    int result = circularWalk(n, s, t, r_0, g, seed, p);
-    cout << result << endl;
+
+    long int n,s,t,r_0,g,seed,p;
+    cin >> n >> s >> t >> r_0 >> g >> seed >> p;
+    long int r[n];r[0] = r_0;
+    for( long int i=1;i<n;i++)
+    { r[i] = ((r[i-1] * g)%p + seed%p) % p ;
+        if(r[i] < 0) r[i] +=p;}
+    vector <vector< long int> > G(n);
+
+    for( long int i=0;i<n;i++)
+    {
+        if( r[i] * 2 + 1 > n)
+        {
+            for( int y=0;y<n;y++)
+                G[i].push_back(y);
+            continue;
+        }
+        long int jl = (i-r[i]);
+        long int jb = (i+r[i]);
+        long int j;
+        for( j=jl; j<=jb; j++)
+        { if(j!=i)
+            G[i].push_back( (j%n) + ((j%n<0) ? n:0) );}
+
+    }
+
+   for(long int i=0;i<n ;i++)
+    { visited.push_back(0); dist.push_back(n+1);}
+
+    BFS(&G, s,t);
+
+    cout << dist[t];
+
     return 0;
 }
